abcebb773613c3dd47f7f6663652c0b0ccbf79a7
notes <notes@sigil.systems>
2025-01-02 19:28:19 -0600

    added stuff

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/styles.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="liquid-background">
+        <div className="liquid-background__content">
+          TEST
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
